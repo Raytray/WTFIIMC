ff440f765995011bf327877591839da7a241e3d4
Raymond Tang <raymond@tang.me>
2013-10-29 21:11:15 -0400

    add event api return

--- WS_Flight/index.php
+++ WS_Flight/index.php
@@ -22,85 +22,104 @@ Flight::route('/events/@id', function($id){
         Flight::render('events', array('title' => 'Events', 'event_id' => $id));
 });
 
-Flight::route('/api/events', function(){
-	include_once('php/dblogin.php');
-	$db_connection = new mysqli($SERVER, $USER, $PASSWORD, $DB);
-	if (mysqli_connect_error()) {
-			echo "Can't connect!";
-			echo "<br>" . mysqli_connect_error();
-			return null;
-	}
-	$sql = "SELECT * FROM events";
-	$results = mysqli_query($db_connection, $sql);
+Flight::route('GET /api/events', function(){
+        include_once('php/dblogin.php');
+        $db_connection = new mysqli($SERVER, $USER, $PASSWORD, $DB);
+        if (mysqli_connect_error()) {
+                echo "Can't connect!";
+                echo "<br>" . mysqli_connect_error();
+                return null;
+        }
+        $sql = "SELECT * FROM events";
+        $results = mysqli_query($db_connection, $sql);
 
+        $json = array();
+
+        while($row = mysqli_fetch_array($results)) {
+            $bus = array('id'=> $row['id'],
+                         'name' => $row['name'],
+                         'event_info'=> $row['event_info'],
+                         'start_datetime' => $row['start_datetime'],
+                         'end_datetime' => $row['end_datetime']);
+            array_push($json, $bus);
+        };
+
+        mysqli_close($db_connection);
+
+        $return = array('results' => $json);
+
+        echo json_encode($return);
 });
 
-Flight::route('GET /api/event/@id', function($id){
-	include_once('php/dblogin.php');
-	$db_connection = new mysqli($SERVER, $USER, $PASSWORD, $DB);
-	if (mysqli_connect_error()) {
-			echo "Can't connect!";
-			echo "<br>" . mysqli_connect_error();
-			return null;
-	}
-	$sql = "SELECT * FROM events where id=" . $id;
-	$results = mysqli_query($db_connection, $sql);
+Flight::route('/api/event/@id', function($id){
+        include_once('php/dblogin.php');
+        $db_connection = new mysqli($SERVER, $USER, $PASSWORD, $DB);
+        if (mysqli_connect_error()) {
+                        echo "Can't connect!";
+                        echo "<br>" . mysqli_connect_error();
+                        return null;
+        }
+        $sql = "SELECT * FROM events where id=" . $id;
+        $results = mysqli_query($db_connection, $sql);
+        mysqli_close($db_connection);
 });
 
 Flight::route('POST /api/new/event', function(){
-	include_once('php/dblogin.php');
-	$db_connection = new mysqli($SERVER, $USER, $PASSWORD, $DB);
-	if (mysqli_connect_error()) {
-		echo "Can't connect!";
-		echo "<br>" . mysqli_connect_error();
-		return null;
-	}
-	$name = $_POST['name'];
-	$eventinfo = $_POST['event_info'];
-	$start = $_POST['start_datetime'];
-	$end = $_POST['end_datetime'];
+        include_once('php/dblogin.php');
+        $db_connection = new mysqli($SERVER, $USER, $PASSWORD, $DB);
+        if (mysqli_connect_error()) {
+                echo "Can't connect!";
+                echo "<br>" . mysqli_connect_error();
+                return null;
+        }
+        $name = $_POST['name'];
+        $eventinfo = $_POST['event_info'];
+        $start = $_POST['start_datetime'];
+        $end = $_POST['end_datetime'];
 
-	if(isset($name)){
-		$ins_stmt = $db_connection->stmt_init();
-		if($ins_stmt->prepare("insert into events (name, event_info, start_datetime, end_datetime) values (?, ?, ?, ?)")){
-			$ins_stmt->bind_param("ssss", $name, $eventinfo, $start, $end);
-			$ins_stmt->execute();
-			$ins_stmt->close();
-		}
-	}
+        if(isset($name)){
+                $ins_stmt = $db_connection->stmt_init();
+                if($ins_stmt->prepare("insert into events (name, event_info, start_datetime, end_datetime) values (?, ?, ?, ?)")){
+                        $ins_stmt->bind_param("ssss", $name, $eventinfo, $start, $end);
+                        $ins_stmt->execute();
+                        $ins_stmt->close();
+                }
+        }
 
-	$stmt = $db_connection->stmt_init();
+        $stmt = $db_connection->stmt_init();
 
-	$stmt->close();
+        $stmt->close();
 
-	$sql = "SELECT * FROM events";
-	$results = mysqli_query($db_connection, $sql);
-	mysqli_close($db_connection);
+        $sql = "SELECT * FROM events";
+        $results = mysqli_query($db_connection, $sql);
+        mysqli_close($db_connection);
 });
 
 Flight::route('POST /api/new/participant', function(){
-	include_once('php/dblogin.php');
+        include_once('php/dblogin.php');
 
-	$db_connection = new mysqli($SERVER, $USER, $PASSWORD, $DB);
-	if (mysqli_connect_error()) {
-		echo "Can't connect!";
-		echo "<br>" . mysqli_connect_error();
-		return null;
-	}
+        $db_connection = new mysqli($SERVER, $USER, $PASSWORD, $DB);
+        if (mysqli_connect_error()) {
+                echo "Can't connect!";
+                echo "<br>" . mysqli_connect_error();
+                return null;
+        }
 
-	$name = $_POST['name'];
-	$can_drive = $_POST['can_drive'];
-	$seats = $_POST['seats'];
-	$event_id = $_POST['event_id'];
-	$start = $_POST['start_datetime'];
-	$end = $_POST['end_datetime'];
+        $name = $_POST['name'];
+        $can_drive = $_POST['can_drive'];
+        $seats = $_POST['seats'];
+        $event_id = $_POST['event_id'];
+        $start = $_POST['start_datetime'];
+        $end = $_POST['end_datetime'];
+        $request = Flight::request();
+        $data = $request->data;
 
-	$sql="insert into participants (name, can_drive, seats, event, start_datetime, end_datetime) values ('$name', '$can_drive', '$seats', '$event_id', '$start', '$end')";
+        $sql="insert into participants (name, can_drive, seats, event, start_datetime, end_datetime) values ('$name', '$can_drive', '$seats', '$event_id', '$start', '$end')";
 
-	$result = mysqli_query($db_connection, $sql);
-	$sql = "SELECT name FROM participants WHERE event = " . $event_id;
-	$results = mysqli_query($db_connection, $sql);
-	mysqli_close($db_connection);
+        $result = mysqli_query($db_connection, $sql);
+        $sql = "SELECT name FROM participants WHERE event = " . $event_id;
+        $results = mysqli_query($db_connection, $sql);
+        mysqli_close($db_connection);
 });
 
 Flight::start();
