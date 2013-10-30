092c7af3a70668aa17e779ddf4244d47e67527c9
Connie Xie <cyx3rb@virginia.edu>
2013-10-29 23:53:52 -0400

    connie is awesome android home screen functionality implemented

--- Android/WTFIIMC/src/com/example/wtfiimc/MainActivity.java
+++ Android/WTFIIMC/src/com/example/wtfiimc/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.DateSorter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,12 +19,18 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends Activity {
 
@@ -38,7 +45,7 @@ public class MainActivity extends Activity {
     String getResult = "untouched!!";
     
     ListView courseList;
-    String webserviceURL = "http://plato.cs.virginia.edu/~cs4720f13cucumber/";
+    String webserviceURL = "http://plato.cs.virginia.edu/~cs4720f13cucumber/api/";
     
     
 	@Override
@@ -63,7 +70,9 @@ public class MainActivity extends Activity {
         			returnResults.setText("Missing required fields");
         		}
         		else {
-        			returnResults.setText(nameField.getText().toString() + " created the event: " + eventField.getText().toString());
+        			//returnResults.setText(nameField.getText().toString() + " created the event: " + eventField.getText().toString());
+        			
+        			new PostNewEvent().execute(eventField.getText().toString(), descField.getText().toString(), startDate.getText().toString(), endDate.getText().toString());
         			
             		Log.i("Web", "submitted");
             		Log.i("Web", nameField.getText().toString());
@@ -72,10 +81,53 @@ public class MainActivity extends Activity {
             		descField.setText("");
             		startDate.setText("");
             		endDate.setText("");
+            		getCurrentEvents();
         		}
         	}
         });
     }
+	private class PostNewEvent extends AsyncTask<String, Integer, String> {
+		@Override
+		protected void onPreExecute() {
+		}
+		@Override
+		protected String doInBackground(String... param){
+			String url = webserviceURL + "new/event";
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(url);
+			// http post
+			try {
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+		        nameValuePairs.add(new BasicNameValuePair("name", param[0]));
+		        nameValuePairs.add(new BasicNameValuePair("event_info", param[1]));
+		        nameValuePairs.add(new BasicNameValuePair("start_datetime", param[2]));
+		        nameValuePairs.add(new BasicNameValuePair("end_datetime", param[3]));
+		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+				HttpResponse response = httpclient.execute(httppost);
+				//HttpEntity entity = response.getEntity();
+				//is = entity.getContent();
+				Log.d("response code", response.getStatusLine().toString());
+				
+
+			} catch (Exception e) {
+				Log.e("LousList", "Error in http connection " + e.toString());
+			}
+			
+			return "Added new event: " + param[0];
+		}
+		@Override
+		protected void onProgressUpdate(Integer... ints) {
+
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// tells the adapter that the underlying data has changed and it
+			// needs to update the view
+			returnResults.setText(result);
+		}
+	}
+	
 	private class GetCurrentEvents extends AsyncTask<String, Integer, String> {
 		@Override
 		protected void onPreExecute() {
@@ -94,6 +146,7 @@ public class MainActivity extends Activity {
 				HttpEntity entity = response.getEntity();
 				is = entity.getContent();
 				getResult = "touched twice";
+				Log.d("response code", response.getStatusLine().toString());
 				
 
 			} catch (Exception e) {
@@ -115,7 +168,19 @@ public class MainActivity extends Activity {
 				Log.e("LousList", "Error converting result " + e.toString());
 			}
 			Log.i("Web", result);
-			return result;
+			StringBuilder jsonToString = new StringBuilder();
+			try{
+				JSONObject jObject = new JSONObject(result);
+				JSONArray resultsArray = jObject.getJSONArray("results");
+				for(int i=resultsArray.length()-1; i>= 0; i--){
+					JSONObject eventObject = resultsArray.getJSONObject(i);
+					jsonToString.append(eventObject.getString("id")+" "+eventObject.getString("name")+"\n");
+				}
+			}
+			catch(Exception e){
+				Log.e("JSON failed", "Error: " +e.toString());
+			}
+			return jsonToString.toString();
 		}
 		@Override
 		protected void onProgressUpdate(Integer... ints) {
@@ -130,7 +195,7 @@ public class MainActivity extends Activity {
 		}
 	}
 	private void getCurrentEvents(){
-		String url = "http://plato.cs.virginia.edu/~cs4720f13cucumber/";
+		String url = webserviceURL + "event";
 		
 		new GetCurrentEvents().execute(url);
 		
