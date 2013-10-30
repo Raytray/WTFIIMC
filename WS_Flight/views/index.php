c72e684eec6582d22a74a2003965b83c4500e33f
Raymond Tang <raymond@tang.me>
2013-10-29 23:53:47 -0400

    wip: initial progress for wtfiimc.less
    * BUG: Right box is down a little for some reason.

--- WS_Flight/views/index.php
+++ WS_Flight/views/index.php
@@ -5,6 +5,7 @@
         <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
         <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
         <link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+        <link href="static/css/wtfiimc.css" rel="stylesheet">
         <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
         <script>
             $(document).ready(function(){
@@ -28,39 +29,42 @@
 
         <div class="container">
             <h1>Who The Fox Is In My Car</h1>
-            <p>
-                <b>Create a new event:</b><br>
-                <form id="event_insert">
-                Event name: <input name="name" type="text" /><br>
-                Event description: <br>
-                <textarea style='margin: 0px; width: 262px; height: 130px;' name="event_info" type="text"></textarea><br>
-                Start Date: <input name="start_datetime" type="text" /><br>
-                End Date: <input name="end_datetime" type="text" /><br>
-                <button type="button" class="btn btn-defualt" id="submitevent">Create Event!</button>
-                </form>
-            </p>
-            <div id="results"></div>
-            <p><b>Current events:</b>
-                <div id="current_events">
-                <?php
-                  include_once('dblogin.php');
-                  $db_connection = new mysqli($SERVER, $USER, $PASSWORD, $DB);
-                  if (mysqli_connect_error()) {
-                      echo "Can't connect!";
-                      echo "<br>" . mysqli_connect_error();
-                      return null;
-                  }
-                  $sql = "SELECT * FROM events";
-                  $results = mysqli_query($db_connection, $sql);
-                  echo '<ol>';
-                  while($row = mysqli_fetch_array($results)) {
-                      echo '<li><a href=/~cs4720f13cucumber/events/' . $row['id'] . '>' . $row['name'] . '</li>';
-                  }
-                  echo '</ol>';
-                  mysqli_close($db_connection);
-                ?>
-                </div>
-            </p>
+            <div class="left_box">
+                <p>
+                    <b>Create a new event:</b><br>
+                    <form id="event_insert">
+                        Event name: <input name="name" type="text" /><br>
+                        Event description: <br>
+                        <textarea style='margin: 0px; width: 262px; height: 130px;' name="event_info" type="text"></textarea><br>
+                        Start Date: <input name="start_datetime" type="text" /><br>
+                        End Date: <input name="end_datetime" type="text" /><br>
+                        <button type="button" class="btn btn-defualt" id="submitevent">Create Event!</button>
+                    </form>
+                </p>
+            </div>
+            <div class="right_box">
+                <p><b>Current events:</b>
+                    <div id="current_events">
+                        <?php
+                          include_once('dblogin.php');
+                          $db_connection = new mysqli($SERVER, $USER, $PASSWORD, $DB);
+                          if (mysqli_connect_error()) {
+                              echo "Can't connect!";
+                              echo "<br>" . mysqli_connect_error();
+                              return null;
+                          }
+                          $sql = "SELECT * FROM events";
+                          $results = mysqli_query($db_connection, $sql);
+                          echo '<ol>';
+                          while($row = mysqli_fetch_array($results)) {
+                              echo '<li><a href=/~cs4720f13cucumber/events/' . $row['id'] . '>' . $row['name'] . '</a></li>';
+                          }
+                          echo '</ol>';
+                          mysqli_close($db_connection);
+                        ?>
+                    </div>
+                </p>
+            </div>
         </div>
     </body>
 
