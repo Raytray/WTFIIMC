f4c1c5cee124281548e1e965831833920eb0548f
Lichard <ryli721@gmail.com>
2013-11-06 17:08:53 -0500

    fix python parameter check. need test by ray.

--- App_Engine/wtfiimc.py
+++ App_Engine/wtfiimc.py
@@ -20,7 +20,7 @@ class event_API(webapp2.RequestHandler):
 
         self.response.headers['Content-Type'] = 'application/json'
         callback = self.request.get('callback')
-        if callback is not None:
+        if not callback:
             callback = self.request.get('callback')
             jsonp_schedule = callback + "(" + schedule + ")"
             self.response.write(jsonp_schedule)
